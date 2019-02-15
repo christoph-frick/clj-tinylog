d382dde791e9c5e05bea79e60b31214c0e583286
Christoph Frick <cf@ofnir.net>
2019-02-15 17:11:45 +0100

    Update versions; add project links; update project name

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject net.ofnir/twig "0.1.0-SNAPSHOT"
+(defproject net.ofnir/clj-tinylog "0.1.0-SNAPSHOT"
   :description "Wrapper for tinylog"
-  :url "TODO"
+  :url "https://github.com/christoph-frick/clj-tinylog"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.tinylog/tinylog "1.3.1"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.tinylog/tinylog "1.3.6"]])
